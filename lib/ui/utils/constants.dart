c040a1db7ebe2435aea116474c5d8158e27cac3c
Nitoan <nitoan159@gmail.com>
2024-01-04 12:53:49 -0500

    second commit

--- lib/ui/utils/constants.dart
+++ lib/ui/utils/constants.dart
@@ -1,2 +1,2 @@
 const String pathProduction = "https://api.themoviedb.org/3";
-const String apiKey = "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";
+const String apiKey = "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";
